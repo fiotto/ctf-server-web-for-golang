3408d82ecf69333cccee97c9da36fe4e174b3673
fiotto <fiotto42@gmail.com>
2019-12-18 23:04:07 +0900

    commit project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ctf-server-web-for-golang
+## ctf-server-web-for-golang
+
+### 注意
+このリポジトリはCTFの問題サーバです。  
+そのため意図的に脆弱性を埋め込んでいます。
+
+### Dockerの起動
+```
+$ docker network create ctf-network
+$ docker-compose up -d
+```
+
+データベースの確認
+```
+$ docker container exec -it mysql /bin/sh
+$ mysql -u [ユーザー名] -p
+```
---
+++ go/src/app.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "strconv"
+  "net/http"
+  "text/template"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
+)
+
+type Page struct {
+  Query string
+  Result []User
+}
+
+type User struct {
+  Id          int
+  FirstName   string
+  LastName    string
+  Job         string
+  DeleteFlag  bool
+}
+
+const PORT = 8080
+
+func createHtml(w http.ResponseWriter, r *http.Request) error {
+  db, err := sql.Open("mysql", "root:password@tcp(mysql:3306)/ctf_db")
+  if err != nil {
+    panic(err.Error())
+  }
+  defer db.Close()
+  
+  query := r.FormValue("q")
+  rows, err := db.Query("SELECT * FROM users WHERE delete_flag=FALSE AND job LIKE '%" + query + "%'")
+
+  if err != nil {
+    return err
+  }
+  defer rows.Close()
+
+  members := []User{}
+  for rows.Next() {
+    user := User{}
+    err = rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Job, &user.DeleteFlag)
+    if err != nil {
+      return err
+    }
+    members = append(members, user)
+  }
+
+  page := Page{query, members}
+  tmpl, err := template.ParseFiles("index.html") 
+  if err != nil {
+    return err
+  }
+
+  err = tmpl.Execute(w, page)
+  if err != nil {
+    return err
+  }
+  
+  return nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  err := createHtml(w, r)
+  if( err != nil ){
+    fmt.Fprintf(w, err.Error())
+  }
+}
+
+func main() {
+  http.HandleFunc("/", viewHandler)
+
+  fmt.Println("serving at port", PORT)
+  err := http.ListenAndServe(":" + strconv.Itoa(PORT), nil)
+  if err != nil {
+    fmt.Fprint(os.Stderr, "ListenAndServe failed.\n", err, "\n")
+  }
+}
